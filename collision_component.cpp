4787bcfc804074261f51b9ebb78b2eac34f705a4
debobbin <you@example.com>
2020-08-01 02:22:13 +0200

    wip

--- collision_component.cpp
+++ collision_component.cpp
@@ -2,6 +2,10 @@
 #include "collision_component.hpp"
 #include "entity.hpp"
 
+bool collision()
+{
+	return false;
+}
 
 CollisionComponent::CollisionComponent(Entity* owner, std::vector<Entity*> colliders)
 : BehaviorComponent(owner), colliders(colliders)
@@ -9,6 +13,10 @@ CollisionComponent::CollisionComponent(Entity* owner, std::vector<Entity*> colli
 
 void CollisionComponent::behave(std::vector<Action*>& actions)
 {
-
+	for (auto collider: colliders){
+		if (collision()){
+			
+		}
+	}
 }
 
